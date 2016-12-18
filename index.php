c604a86d58076d29d5661c517c182f814bd59500
lys0829 <yes9830316@gmail.com>
2016-12-18 20:10:48 +0800

    可將多個比賽放到同資料夾中

--- index.php
+++ index.php
@@ -16,6 +16,8 @@ if (!empty($QUEST)) { //remove first '/'
 }
 $QUEST = explode('/', $QUEST);
 
+$contestn="";
+$classn="";
 if(empty($QUEST[0])){
     if(!isset($_POST['change'])){
         exit ('please add class name!!!');
@@ -26,16 +28,30 @@ if(empty($QUEST[0])){
 }
 else{
     $classn = (string)$QUEST[0];
+    
+}
+if(empty($QUEST[1])){
+    if(!isset($_POST['change'])){
+        exit ('please add contest name!!!');
+    }
+    else{
+        $contestn = json_decode($_POST['change'])->contest_name;
+    }
+}
+else{
+    $contestn = (string)$QUEST[1];
 }
 if(!is_dir($classn))exit ('no class');
-$contest = json_decode(file_read($classn.'/contest.json'));
+if(!is_dir($classn.'/'.$contestn))exit ('no contest');
+$dir = $classn.'/'.$contestn; 
+$contest = json_decode(file_read($dir.'/contest.json'));
 $pr_num = $contest->pro_num;
 $title = $contest->name;
 $problem_list = $contest->problem;
-$user_list = json_decode(file_read($classn.'/userlist.json'));
+$user_list = json_decode(file_read($dir.'/userlist.json'));
 $sa = [];
 foreach($user_list as $u){
-    $user_list[$u->id]->score = json_decode(file_read($classn."/users/user{$u->id}.json"));
+    $user_list[$u->id]->score = json_decode(file_read($dir."/users/user{$u->id}.json"));
     $user_list[$u->id]->scoresum = 0;
     foreach($user_list[$u->id]->score as $sc){
         $user_list[$u->id]->scoresum += $sc;
@@ -45,27 +61,18 @@ foreach($user_list as $u){
 
 require_once "update.php";
 
-arsort($sa);
-$urankl = [];
-$i=0;
 $uranknum = 1;
 $uranknumsh = 1;
 $upuscore = 0;
-foreach($sa as $key => $val){
-    //echo "$key <p>";
+$i=0;
+foreach($contest->ranklist as $key){
     if($user_list[$key]->scoresum != $upuscore && $i!=0){
         $uranknumsh = $uranknum;
     }
     $user_list[$key]->rank = $uranknumsh;
-    $urankl[$i] = $key;
-    $upuscore = $val;
+    $upuscore = $user_list[$key]->scoresum;
     $uranknum++;
     $i++;
 }
-$contest->ranklist = $urankl;
-file_create($classn.'/contest.json',json_encode($contest));
-//$urankl = json_encode($urankl);
-//echo $urankl;
 
-//echo "loaded";
 require_once 'rank.php';
--- update.php
+++ update.php
@@ -24,6 +24,7 @@ if(isset($_POST['change'])){
     }
     if($info->pid==-1)exit('no problem');
     if($info->change_score>$user_list[$info->uid]->score[$info->pid])$user_list[$info->uid]->score[$info->pid] = (int)$info->change_score;
+    file_create($dir."/users/user{$info->uid}.json",json_encode($user_list[$info->uid]->score));
     
     $user_list[$info->uid]->scoresum = 0;
     foreach($user_list[$info->uid]->score as $sc){
@@ -31,7 +32,6 @@ if(isset($_POST['change'])){
     }
     
     $sa[$info->uid] = $user_list[$info->uid]->scoresum;
-    file_create($classn."/users/user{$info->uid}.json",json_encode($user_list[$info->uid]->score));
     
     arsort($sa);
     $urankl = [];
@@ -39,18 +39,25 @@ if(isset($_POST['change'])){
     $uranknum = 1;
     $uranknumsh = 1;
     $upuscore = 0;
+    $user_list_save = [];
+    for($j=0;$j<count($user_list);$j++)$user_list_save[$j] = new stdclass;
     foreach($sa as $key => $val){
         //echo "$key <p>";
         if($user_list[$key]->scoresum != $upuscore && $i!=0){
             $uranknumsh = $uranknum;
         }
         $user_list[$key]->rank = $uranknumsh;
+        $user_list_save[$key]->vpl_uid = $user_list[$key]->vpl_uid;
+        $user_list_save[$key]->rank = $user_list[$key]->rank;
+        $user_list_save[$key]->id = $user_list[$key]->id;
+        $user_list_save[$key]->name = $user_list[$key]->name;
         $urankl[$i] = $key;
         $upuscore = $val;
         $uranknum++;
         $i++;
     }
     $contest->ranklist = $urankl;
-    file_create($classn.'/contest.json',json_encode($contest));
+    file_create($dir.'/contest.json',json_encode($contest));
+    //file_create($classn.'/userlist.json',json_encode($user_list_save));
     exit('SUCC');
 }
